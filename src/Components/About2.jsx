2d48120aa493138b3f0b7648ef2d27d791cb814d
gitAlmair <160695360+kramalmr@users.noreply.github.com>
2025-04-28 09:59:07 +0700

    updet

--- src/Components/About2.jsx
+++ src/Components/About2.jsx
@@ -26,33 +26,43 @@ export default function About2() {
             <h1 className="text-2xl lg:text-4xl font-semibold lg:w-5/12 text-white">
               Saat ini, 608 jenis Menempati taman Nasional Ujung Kulon
             </h1>
-            <p className="text-gray-700 lg:w-7/12 bg-white p-4 h-fit lg:text-lg">
+            <motion.p
+              className="text-gray-700 lg:w-7/12 bg-white p-4 h-fit lg:text-lg"
+              initial={{ opacity: 0, y: 50 }}
+              whileInView={{ opacity: 1, y: 0 }}
+              transition={{ duration: 0.7, delay: 0.6 }}
+            >
               Taman Nasional Ujung Kulon memiliki julukan sebagai "suaka badak
               Jawa" atau "rumah terakhir badak Jawa" karena merupakan habitat
               alami terakhir bagi spesies badak bercula satu ini di Indonesia.
               🦏🦏🦏
-            </p>
+            </motion.p>
           </div>
         </motion.div>
 
         {/* Flora Section */}
         <motion.div
           className="flex flex-col gap-5 bg-blue-300 p-10 lg:p-12 lg:w-1/2 bg-[url(/about/flora.jpg)] bg-cover border-8 border-white"
-          initial={{ opacity: 0, x: 50 }}
-          whileInView={{ opacity: 1, x: 0 }}
+          initial={{ opacity: 0, scale: 0.95 }}
+          whileInView={{ opacity: 1, scale: 1 }}
           transition={{ duration: 0.7, delay: 0.4 }}
         >
           <div className="flex flex-col gap-3 lg:gap-7 lg:flex-row">
             <h1 className="text-2xl lg:text-4xl font-semibold lg:w-5/12 text-white">
               Dan Sekitar 700 jenis Flora yang dapat ditemukan disini.
             </h1>
-            <p className="text-gray-700 lg:w-7/12 bg-white p-4 h-fit lg:text-lg">
+            <motion.p
+              className="text-gray-700 lg:w-7/12 bg-white p-4 h-fit lg:text-lg"
+              initial={{ opacity: 0, y: 50 }}
+              whileInView={{ opacity: 1, y: 0 }}
+              transition={{ duration: 0.7, delay: 0.6 }}
+            >
               Beberapa tumbuhan yang terkenal di kawasan ini antara lain
               Rafflesia arnoldii (bunga bangkai), meranti, jelutung, nyatoh, dan
               berbagai jenis anggrek. Taman Nasional ini memiliki sekitar 700
               jenis tumbuhan yang terlindungi, dengan 57 di antaranya termasuk
               jenis langka.
-            </p>
+            </motion.p>
           </div>
         </motion.div>
       </div>
